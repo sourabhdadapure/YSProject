c00f430b4f63eef6ebf192ca38fb54305114ce3b
sourabhdadapure <sourabhdadapure@gmail.com>
2020-04-05 12:20:18 -0400

    updates barGraph

---
+++ src/components/BarGraph/BarGraph.tsx
@@ -0,0 +1,182 @@
+import * as React from "react";
+import { View } from "react-native";
+import Svg, { Circle, Rect, Pattern, Line, Path } from "react-native-svg";
+import YAxisLabels from "./YAxisLabels";
+import Graph from "./Graph";
+
+export interface BarGraphYData {
+  start: number;
+  end?: number;
+}
+interface GraphYAxisData {
+  label: string;
+  position: number;
+}
+
+export interface yAxis {
+  start: number;
+  end?: number;
+}
+
+export type BarType = "fair" | "good";
+
+export interface YAxisType {
+  values: number[];
+  type: BarType[];
+}
+
+export interface BarGraphProperties {
+  // value: number;
+  height: number;
+  width: number;
+  // xAxisLabels: TimelineData;
+  yAxisValues: YAxisType;
+  verticalPadding: number;
+}
+export type Point = number[];
+
+export default class BarGraph extends React.Component<BarGraphProperties> {
+  render() {
+    const { width, verticalPadding, yAxisValues } = this.props;
+    const yAxes: yAxis[] = [];
+    const yData: GraphYAxisData[] = [];
+    const graphYMax = Math.max.apply(null, yAxisValues.values);
+    const graphYMin = Math.min.apply(null, yAxisValues.values);
+    const maxYAxis =
+      Math.ceil(graphYMax / 20) * 20 <= 20
+        ? 80
+        : Math.ceil(graphYMax / 20) * 20;
+    const minYAxis =
+      Math.floor(graphYMin / 20) * 20 <= 20
+        ? 0
+        : Math.floor(graphYMin / 20) * 20;
+
+    for (let i = minYAxis; i <= maxYAxis; i += 20) {
+      yAxes.push({
+        start: i,
+      });
+      yData.push({
+        position: i,
+        label: i.toFixed(0),
+      });
+    }
+
+    const pad = verticalPadding !== undefined ? this.props.verticalPadding : 5;
+    const height = this.props.height - pad * 2;
+    const yTotal = maxYAxis - minYAxis;
+    const xStepSize = width / yAxisValues.values.length - 0.5;
+
+    const lineColor = "blue"; //TODO: update later
+    const lineOpacity = 0.5;
+    const barWidth = 3;
+    let svgPath;
+    let graph = new Graph();
+    let points: Point[] = [];
+    let hasData = false;
+    let x = 0;
+    let y = 0;
+    for (let idx = 0; idx < yAxisValues.values.length; idx++) {
+      const item = yAxisValues.values[idx];
+      hasData = true;
+      const h = Math.max((item - minYAxis) / yTotal, 0.01);
+      y = pad + (1 - h) * height + 10;
+      x = idx * xStepSize + (xStepSize - barWidth) / 2 + 10;
+      if (hasData) points.push([x, y]);
+    }
+    if (points.length > 0) {
+      svgPath = graph.svgPath(points);
+    }
+
+    return (
+      <View style={{ flex: 1 }}>
+        <View style={{ flexDirection: "row" }}>
+          <YAxisLabels
+            width={40}
+            height={height}
+            verticalPadding={10}
+            max={maxYAxis}
+            min={minYAxis}
+            data={yData}
+          />
+          <Svg
+            style={{ backgroundColor: "grey" }}
+            height={height}
+            width={width}>
+            <Pattern
+              id="RangePattern"
+              patternUnits="userSpaceOnUse"
+              x="-10"
+              y="-10"
+              width="10"
+              height="10">
+              {/* {svgPath && (
+                <Path
+                  stroke="yellow" //TODO: update later
+                  opacity={1}
+                  strokeWidth={1.5}
+                  fill="transparent"
+                  d={svgPath}
+                />
+              )} */}
+            </Pattern>
+            {yAxisValues.values.map((item, idx) => {
+              const h = Math.max((item - minYAxis) / yTotal, 0.01);
+              const y = pad + (1 - h) * height;
+              return (
+                // <Circle
+                //   key={"bar-" + idx}
+                //   x={idx * xStepSize + (xStepSize - barWidth) / 2}
+                //   y={y}
+                //   r={barWidth}
+                //   stroke="orange" //TODO: update later
+                //   strokeWidth={2}
+                //   fill="pink" //TODO: update later
+                // />
+                <Rect
+                  key={"bar-" + idx}
+                  x={idx * xStepSize + (xStepSize - barWidth) / 2}
+                  y={y}
+                  rx={barWidth / 2}
+                  width={barWidth}
+                  height={h * height}
+                  fill={"blue"}
+                />
+              );
+            })}
+
+            {yAxes.map((item, idx) => {
+              const y =
+                pad + height + ((item.start - minYAxis) / yTotal) * -height;
+              if (item.end !== undefined) {
+                const rangeHeight = ((item.end - item.start) / yTotal) * height;
+                return (
+                  <Rect
+                    key={"line-" + idx}
+                    fill="url(#RangePattern)"
+                    x={0}
+                    y={y - rangeHeight}
+                    width={width}
+                    height={rangeHeight}
+                  />
+                );
+              }
+              return (
+                <Line
+                  key={"line-" + idx}
+                  stroke={lineColor}
+                  strokeWidth={1}
+                  strokeDasharray="0.8"
+                  opacity={lineOpacity}
+                  x1={0}
+                  y1={y}
+                  x2={width}
+                  y2={y}
+                />
+              );
+            })}
+          </Svg>
+        </View>
+      </View>
+    );
+  }
+}
