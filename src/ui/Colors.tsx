62c3aaa16ac646e9032b0832c698325e49d5a339
sourabhdadapure <sourabhdadapure@gmail.com>
2020-04-05 12:46:14 -0400

    Adds UI

---
+++ src/ui/Colors.tsx
@@ -0,0 +1,31 @@
+export const Colors = {
+  Background: {
+    Picker: "#141414",
+    Delete: "#ff0000",
+    Card: "#19191c",
+  },
+  Buttons: {
+    Primary: "#78CCFB",
+    PrimaryShadow: "#B1DEFC",
+    White: "#FFFFFF",
+    Blue: "#5289fe",
+    CTA: "#474951",
+    DarkGray: "#19191C",
+    Gray: "#828994",
+    Active: "#5289fe",
+  },
+  Icons: {
+    White: "#ffffff",
+    Gray: "#7a7c86",
+    Black: "#000000",
+    Blue: "#5289fe",
+  },
+
+  Labels: {
+    White: "#ffffff",
+    Gray: "#98A0B4",
+    DarkGray: "#7A7C86",
+    Link: "#5289fe",
+    Black: "#000000",
+  },
+};
